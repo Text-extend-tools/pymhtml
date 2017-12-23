d57410eddf2294ec387e2833d6fb35af88ba36c5
звездочёт <mykaralw@yandex.ru>
2017-12-23 14:36:36 +0300

    0.0.1
    Utilities

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, Extension
 #from distutils.core import setup, Extension
 
-setup(name='libmhtml',
-	description='MHTML library',
+setup(name='pymhtml',
+	description='MHTML tools',
 	author='Chema Gonzalez',
 	author_email='chema@cal.berkeley.edu',
 	url="http://github.com/chemag/libmhtml.py",
 	version='0.0.1',
 	#py_modules=['magic'],
-	long_description="""This module provides an MHTML creator and parser.
-""",
+	long_description="""This module provides an MHTML creator and parser.""",
 	keywords="mhtml mht",
 	license="BSD",
+	install_requires = [
+		'magic',
+		'urlgrabber',
+	],
+	scripts = ['pymhtml']
 	)
 
