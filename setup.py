1611638d507c5b9a485388890f928088d34c4601
звездочёт <mykaralw@yandex.ru>
2017-12-25 04:21:28 +0300

    0.0.2
    pyformat -a -i --remove-all-unused-imports --remove-unused-variables

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup, Extension
+from setuptools import setup
 #from distutils.core import setup, Extension
 
 setup(name='pymhtml',
-	description='MHTML tools',
-	author='Chema Gonzalez',
-	author_email='chema@cal.berkeley.edu',
-	url="http://github.com/chemag/libmhtml.py",
-	version='0.0.1',
-	#py_modules=['magic'],
-	long_description="""This module provides an MHTML creator and parser.""",
-	keywords="mhtml mht",
-	license="BSD",
-	install_requires = [
-		'magic',
-		'urlgrabber',
-	],
-	scripts = ['pymhtml']
-	)
-
+      description='MHTML tools',
+      author='Chema Gonzalez',
+      author_email='chema@cal.berkeley.edu',
+      url='http://github.com/chemag/libmhtml.py',
+      version='0.0.2',
+      # py_modules=['magic'],
+      long_description="""This module provides an MHTML creator and parser.""",
+      keywords='mhtml mht',
+      license='BSD',
+      install_requires=[
+          'magic',
+          'urlgrabber',
+      ],
+      scripts=['pymhtml']
+      )
